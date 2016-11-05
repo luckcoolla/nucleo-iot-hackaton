65759dc14323f3290226a7142d4d762c536cc9aa
Kirill Vashilo <luckcoolla@gmail.com>
2016-11-05 13:49:43 +0300

    boards list separator

--- nucleo-spectre-android/app/src/main/java/by/iot/nucleo/spectre/getyoursensors/BoardListActivity.java
+++ nucleo-spectre-android/app/src/main/java/by/iot/nucleo/spectre/getyoursensors/BoardListActivity.java
@@ -2,6 +2,8 @@ package by.iot.nucleo.spectre.getyoursensors;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
@@ -77,6 +79,7 @@ public class BoardListActivity extends BaseActivity {
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.addItemDecoration(new SimpleDividerItemDecoration(this));
         recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(
                 new ArrayList<Board>(DataManager.ITEM_MAP.values())));
     }
@@ -146,6 +149,33 @@ public class BoardListActivity extends BaseActivity {
             @Override
             public String toString() {
                 return super.toString() + " '" + mContentView.getText() + "'";
+            }
+        }
+    }
+
+    private class SimpleDividerItemDecoration extends RecyclerView.ItemDecoration {
+        private Drawable mDivider;
+
+        public SimpleDividerItemDecoration(Context context) {
+            mDivider = context.getResources().getDrawable(R.drawable.line_divider);
+        }
+
+        @Override
+        public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+            int left = parent.getPaddingLeft();
+            int right = parent.getWidth() - parent.getPaddingRight();
+
+            int childCount = parent.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                View child = parent.getChildAt(i);
+
+                RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+                int top = child.getBottom() + params.bottomMargin;
+                int bottom = top + mDivider.getIntrinsicHeight();
+
+                mDivider.setBounds(left, top, right, bottom);
+                mDivider.draw(c);
             }
         }
     }
